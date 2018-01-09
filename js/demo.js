68d0a482af92328dade4e13590add522ea39417d
JassonNoong <nongdewang@qq.com>
2018-01-09 17:17:15 +0800

    创建vue案例

---
+++ js/demo.js
@@ -0,0 +1,29 @@
+new Vue({
+    el:"#app3",
+    data:{
+        newPage:'<p style=color:red>你好,这是一个新网页</p>'
+    }
+})
+
+
+new Vue({
+    el:"#app4",
+    data:{
+        words:'i know you are a good person!',
+        ok:true
+    }
+})
+
+new Vue({
+    el:"#app2",
+    data:{
+        class1:false
+    }
+})
+
+        new Vue({
+    el: "#app",
+    data: {
+        message: "hello,world"
+    }
+})
